7b19c21c15b2ac5eac4d526d87a55b67a86aa64e
Louis <louiscresswell33@gmail.com>
2020-03-11 14:22:08 +1300

    Added Player Turns & Movement Limits

---
+++ BEUP/Assets/GameManager/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private bool playerTurn = true;
+
+    public bool PlayerTurn { get => playerTurn; set => playerTurn = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playerTurn)
+        {
+            playerTurn = true;
+            FindObjectOfType<PlayerInput>().StartPlayerTurn();
+        } 
+    }
+}
